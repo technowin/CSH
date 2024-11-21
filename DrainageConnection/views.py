6249f186acc346dd2981ac6458d6f347cbc74e13
technowin <technowinitinfra@gmail.com>
2024-11-21 13:57:31 +0530

    fdfd

--- DrainageConnection/views.py
+++ DrainageConnection/views.py
@@ -174,6 +174,11 @@ def matrix_flow(request):
                 if (status == 3 or status == 4) and (ref == 'scrutiny'):
                     doc_ids = request.POST.getlist('doc_ids')
                     rej_res = request.POST.get('rej_res')
+                    if rej_res!='' and status in [4]:
+                        internal_user_comments.objects.create(
+                                workflow=wf, comments=rej_res,
+                                created_at=datetime.now(),created_by=str(user),updated_at=datetime.now(),updated_by=str(user)
+                        )  
                     for doc_id in doc_ids:
                         if doc_id !='':
                             doc_id = decrypt_parameter(doc_id)
@@ -191,15 +196,21 @@ def matrix_flow(request):
                     if cheklist_upl_file and inspection_upl_file:
                         file_resp = internal_docs_upload(cheklist_upl_file,role_id,user,wf,ser)
                         file_resp = internal_docs_upload(inspection_upl_file,role_id,user,wf,ser)
-                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user])
+                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user,''])
                     if r[0][0] not in (""):
                         messages.success(request, str(r[0][0]))
                     else: messages.error(request, 'Oops...! Something went wrong!')
                 elif status == 10 and ref == 'certificate':
+                    iss_remark = request.POST.get('iss_remark')
+                    if iss_remark!='':
+                        internal_user_comments.objects.create(
+                                workflow=wf, comments=iss_remark,
+                                created_at=datetime.now(),created_by=str(user),updated_at=datetime.now(),updated_by=str(user)
+                        )  
                     certificate_upl_file = request.FILES.get('certificate_upl_file')
                     if certificate_upl_file:
                         file_resp = internal_docs_upload(certificate_upl_file,role_id,user,wf,ser)
-                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user])
+                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user,iss_remark])
                     fui = workflow_details.objects.filter(id=wf_id).first()
                     form_user_id = fui.form_user_id
                     file_resp = citizen_docs_upload(certificate_upl_file,form_user_id,form_id,user,ser)
@@ -207,7 +218,13 @@ def matrix_flow(request):
                         messages.success(request, str(r[0][0]))
                     else: messages.error(request, 'Oops...! Something went wrong!')
                 else:
-                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user])
+                    f_remark = request.POST.get('f_remark')
+                    if f_remark!='' and status in [6, 7]:
+                        internal_user_comments.objects.create(
+                                workflow=wf, comments=f_remark,
+                                created_at=datetime.now(),created_by=str(user),updated_at=datetime.now(),updated_by=str(user)
+                        )  
+                    r = callproc("stp_post_workflow", [wf_id,form_id,status,ref,ser,user,f_remark])
                     if r[0][0] not in (""):
                         messages.success(request, str(r[0][0]))
                     else: messages.error(request, 'Oops...! Something went wrong!')
@@ -252,7 +269,7 @@ def internal_docs_upload(file,role_id,user,wf,ser):
         )  
         file_resp =  f"File '{file.name}' has been inserted."
     return file_resp
-
+ 
 def citizen_docs_upload(file,user,form_id,created_by,ser):
     file_resp = None
     doc = document_master.objects.get(doc_id=15)
